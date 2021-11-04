80b707a719f4a4c328eadd165dc9ef829cb11b46
mmikhak01 <mmikhak01@gmail.com>
2021-11-04 14:35:21 +0330

    init

--- app1/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    context = {'dams':1}
-    return render(request, 'index.html', context)
--- create app.bat
+++
@@ -1,3 +0,0 @@
-"C:\Users\mahmood-s\AppData\Local\Programs\Python\Python36\python.exe" manage.py startapp app1
-
-pause
